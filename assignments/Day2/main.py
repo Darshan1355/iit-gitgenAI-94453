52bb0e4909b20b5e6e96fbf37f2f9a4433736ce0
Darshan1355 <darshanamane083@gmail.com>
2025-12-12 12:43:08 +0530

    day2 2nd

--- assignments/Day2/main.py
+++ assignments/Day2/main.py
@@ -1,3 +1,7 @@
-import ex1
-result=ex1.sqr_area(4)
-print("result: ",result)
+import mathUtil as ex1
+sq_result=ex1.sqr_area(4)
+print("square area: ",sq_result)
+cir_result=ex1.cir_area(3)
+print("circle area: ",cir_result)
+rec_area=ex1.rect_area(3,4)
+print("rect area: ",rec_area)
--- assignments/Day2/mathUtil.py
+++ assignments/Day2/mathUtil.py
@@ -5,3 +5,7 @@ def rect_area(len, br):
 def sqr_area(edge):
     area=edge*edge
     print("sqr area: ",area)
+
+def cir_area(rad):
+    area=3.14*rad*rad
+    print("cir area: ",area)    
